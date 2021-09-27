b8fc730673cdac4a64f4565827c65ad43dc1dadf
Daniel Oliveira <danielsapup3@gmail.com>
2021-09-27 20:22:37 -0300

    Tela de acesso ao Chat

--- front_end-chat/src/App.js
+++ front_end-chat/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [logado, setLogado] = useState(false);
+  const [nome, setNome] = useState("");
+  const [sala, setSala] = useState("");
+
+  const conectarSala = () => {
+    console.log("Acessou a sala " + sala + " com o usuário " + nome);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Chat - Sapup3</h1>
+      {!logado ? 
+      <>
+        <label>Nome: </label>
+        <input type="text" placeholder="Nome" name="nome" value={nome} onChange={(texto) => {setNome(texto.target.value)}}/><br/><br/>
+
+        <label>Sala: </label>
+        {/*<input type="text" placeholder="Sala" value={sala} onChange={(text) => {setSala(text.target.value)}}/> */}
+
+        <select name="sala" value={sala} onChange={texto => setSala(texto.target.value)}>
+          <option value="">Selecione</option>
+          <option value="1">Node Js</option>
+          <option value="2">React Js</option>
+          <option value="3">React Native</option>
+          <option value="4">PHP</option>
+        </select><br/><br/>
+
+        <button onClick={conectarSala}>Acessar</button>
+      </>
+      : "Logado"}
     </div>
   );
 }
