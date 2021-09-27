bb2d8ce635a11b1aeeed168b6992dc7f703fdc16
Daniel Oliveira <danielsapup3@gmail.com>
2021-09-27 16:24:23 -0300

    Front_End criado

--- back_end/app.js
+++ back_end/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const socket = require('socket.io');
+const cors = require('cors');
 const app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    app.use(cors());
+    next();
+  });
  
 app.get('/', function (req, res) {
   res.send('Olá, Mundo! - #Sapup3');
@@ -10,8 +19,8 @@ const server = app.listen(8080, () => {
     console.log("Servidor iniciado na porta 8080: http://localhost:8080/");
 });
 
-io = socket(server);
+io = socket(server, {cors: {origin: "*"}});
 
 io.on("connection", (socket) => {
     console.log(socket.io);
-})
+});
